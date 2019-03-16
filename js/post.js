e5b177c80f22ec1dd70d9e8fbaa56e86bc3b82b8
nikhil <nikhilbh3366@gmail.com>
2019-03-16 21:45:48 +0530

    Part-C Complete

--- js/post.js
+++ js/post.js
@@ -1,5 +1,3 @@
-var comment = false;
-
 function commentFunction() {
     
     var textarea = document.getElementById("commentarea");
@@ -18,14 +16,12 @@ function commentFunction() {
     
     var maindiv = document.getElementById("maincommentsection");
     
-    if(flag == false) {
-        maindiv.removeChild(document.getElementsByClassName("comments")[0]);
-        
-        flag = true;
-        
-    }
+    if(textarea.value != "") {
     
-    maindiv.insertBefore(div,maindiv.childNodes[0]);
+        maindiv.insertBefore(div,maindiv.childNodes[0]);
+        textarea.value = "";
+    
+    }
     
 }
 
@@ -38,10 +34,10 @@ function likeFunction() {
     var p = document.getElementById("likepara");
     
     if(likecounter == 1) {
-        p.innerHTML = "1 person like this!";
+        p.innerHTML = "1 person likes this!";
     }
     else {
-        p.innerHTML= likecounter + " people have like this!";
+        p.innerHTML= likecounter + " people have liked this!";
     }
     
     var btn = document.getElementById("likebtn");
@@ -58,6 +54,10 @@ function editFunction() {
     
     var div = document.getElementsByClassName("blogText")[0];
     
+    var divtitle = document.getElementById('blogTitle');
+    
+    var h4 = document.getElementById('blogTitleNew');
+    
     if(flag == false) {
     
         btn.innerHTML = "Save <i class='fa fa-save'></i>";
@@ -70,13 +70,27 @@ function editFunction() {
 
         var textarea = document.createElement('textarea');
 
-        textarea.setAttribute("rows","14");
+        textarea.setAttribute("rows","12");
 
         textarea.setAttribute("style","width: 100%");
 
         textarea.value = para;
         
         textarea.id = "para-area";
+        
+        var heading = h4.innerHTML;
+        
+        divtitle.removeChild(h4);
+        
+        var headarea = document.createElement('textarea');
+        
+        headarea.id = 'heading';
+  
+        headarea.setAttribute("rows","1");
+        
+        headarea.value = heading;
+        
+        divtitle.appendChild(headarea);
 
         div.appendChild(textarea);
         
@@ -90,18 +104,29 @@ function editFunction() {
         
         var area = document.getElementById("para-area");
         
+        var harea = document.getElementById("heading");
+        
         var p = document.createElement('p');
         
+        var h4 = document.createElement('h4');
+        
         p.innerHTML = area.value;
+        
+        divtitle.removeChild(harea);
         
         div.removeChild(area);
         
         p.id = "blogBody";
         
+        h4.id = "blogTitleNew";
+        
+        h4.innerHTML = harea.value;
+        
+        divtitle.appendChild(h4);
+        
         div.appendChild(p);
         
         flag = false;
-        
     }
     
 }
